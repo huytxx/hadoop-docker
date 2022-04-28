eae1a890597463b117f4656628d82eb5799abce3
Xuan Huy <xuanhuy900@gmail.com>
2022-04-28 14:01:56 +0700

    add volumne hadoop

--- start-container.sh
+++ start-container.sh
@@ -8,7 +8,7 @@ N=${1:-3}
 #sudo docker rm -f hadoop-master &> /dev/null
 echo "start hadoop-master container..."
 sudo docker run -itd \
-				-v ./home/volumns:/
+				--mount source=hadoop,target=/ \
                 --net=hadoop \
                 -p 50070:50070 \
                 -p 8088:8088 \
